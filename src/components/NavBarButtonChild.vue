c61c5d1d1f8fe532558f893af13bf5929c564176
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-11 21:25:12 +0100

    create nav bar button child

---
+++ src/components/NavBarButtonChild.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+<template>
+  <button
+    class="rounded-xl px-4 py-3 bg-green-700 w-full text-left flex space-x-3 text-sm hover:bg-opacity-70 hover:translate-x-2 transition-all"
+  >
+    <slot />
+    <span>{{ title }}</span>
+  </button>
+</template>
+<style>
+@import "bootstrap-icons/font/bootstrap-icons.css";
+</style>
