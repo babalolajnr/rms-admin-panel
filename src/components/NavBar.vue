32f60c824c6aedd7f2e9c65cdb2b0a138350c484
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-12 13:05:27 +0100

    add dynamic nav buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,85 +3,110 @@ import { ref } from "vue";
 import NavBarButton from "./NavBarButton.vue";
 import NavBarButtonChild from "./NavBarButtonChild.vue";
 
-const schoolState = ref(false);
-
-function toggleSchoolTree() {
-  schoolState.value = !schoolState.value;
-}
+const navBarButtons = ref([
+  {
+    title: "Dashboard",
+    tree: false,
+    icon: "bi bi-speedometer2",
+    active: false,
+    toggleActive: () => {
+      return;
+    },
+    children: null,
+  },
+  {
+    title: "School",
+    tree: true,
+    icon: "bi bi-building",
+    active: false,
+    toggleActive: function () {
+      this.active = !this.active;
+    },
+    children: [
+      {
+        title: "Classes",
+        icon: "bi bi-collection",
+      },
+      {
+        title: "Sessions",
+        icon: "bi bi-calendar3",
+      },
+      {
+        title: "Periods",
+        icon: "bi bi-calendar3",
+      },
+      {
+        title: "Fees",
+        icon: "bi bi-cash-coin",
+      },
+      {
+        title: "Branches",
+        icon: "bi bi-alt",
+      },
+      {
+        title: "Terms",
+        icon: "bi bi-stopwatch",
+      },
+      {
+        title: "Subjects",
+        icon: "bi bi-book",
+      },
+      {
+        title: "Teachers",
+        icon: "bi bi-person-workspace",
+      },
+    ],
+  },
+  {
+    title: "Students",
+    tree: true,
+    icon: "bi bi-mortarboard",
+    children: [],
+    active: false,
+    toggleActive: function () {
+      console.log("clicked");
+    },
+  },
+  {
+    title: "App",
+    tree: true,
+    icon: "bi bi-nut",
+    children: [],
+    active: false,
+    toggleActive: function () {
+      return;
+    },
+  },
+]);
 </script>
 <template>
   <nav class="basis-1/6">
     <div class="flex flex-col space-y-16 mx-5 my-5">
       <div>Radiant Minds School</div>
-      <ul class="flex flex-col space-y-5">
-        <li class="">
-          <NavBarButton title="Dashboard">
-            <i class="bi bi-speedometer2"></i>
-          </NavBarButton>
-        </li>
-        <li class="flex flex-col space-y-2">
-          <NavBarButton
-            title="School"
-            tree="true"
-            v-on:click="toggleSchoolTree"
-          >
-            <i class="bi bi-building"></i>
+      <ul class="flex flex-col space-y-3">
+        <li
+          v-for="navBarButton in navBarButtons"
+          :key="navBarButton.title"
+          v-on:click="navBarButton.toggleActive()"
+        >
+          <NavBarButton :title="navBarButton.title" :tree="navBarButton.tree">
+            <i :class="navBarButton.icon"></i>
           </NavBarButton>
           <ul
-            class="ml-2 flex flex-col space-y-2 transition-all"
-            :class="{ hidden: !schoolState }"
+            v-if="navBarButton.tree"
+            class="flex flex-col space-y-1"
+            :class="{ hidden: navBarButton.active }"
           >
-            <li>
-              <NavBarButtonChild title="Academic Session">
-                <i class="bi bi-calendar3"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Period">
-                <i class="bi bi-hourglass-split"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Fee">
-                <i class="bi bi-cash-coin"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Classroom">
-                <i class="bi bi-collection"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Branches">
-                <i class="bi bi-alt"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Terms">
-                <i class="bi bi-stopwatch"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Subject">
-                <i class="bi bi-book"></i>
-              </NavBarButtonChild>
-            </li>
-            <li>
-              <NavBarButtonChild title="Teacher">
-                <i class="bi bi-person-workspace"></i>
+            <li
+              v-for="navBarButtonChild in navBarButton.children"
+              :key="navBarButtonChild.title"
+              class="mt-2"
+            >
+              <NavBarButtonChild :title="navBarButtonChild.title">
+                <i :class="navBarButtonChild.icon"></i>
               </NavBarButtonChild>
             </li>
           </ul>
-        </li>
-        <li>
-          <NavBarButton title="Student" tree="true">
-            <i class="bi bi-mortarboard"></i>
-          </NavBarButton>
-        </li>
-        <li>
-          <NavBarButton title="App" tree="true">
-            <i class="bi bi-nut"></i>
-          </NavBarButton>
         </li>
       </ul>
     </div>
