863fb2cb8d9d040d4de6755462b689caed8edced
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 12:33:46 +0100

    remove aside

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import NavBar from "./components/NavBar.vue";
 <template>
   <div class="flex h-screen">
     <NavBar />
-    <div class="basis-1/2 border-x border-gray-700">Main body</div>
-    <div class="grow">aside</div>
+    <div class="grow border-x border-gray-700">Main body</div>
   </div>
 </template>
