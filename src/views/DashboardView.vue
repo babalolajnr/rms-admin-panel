bc865285954a894603ef731181034a47b3b2d7ba
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 14:23:04 +0100

    add info card

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,4 +1,15 @@
 <template>
-  <div>Hello</div>
+  <div class="flex flex-col mx-16 my-8">
+    <div class="flex">
+      <div
+        class="w-56 h-32 rounded-xl bg-purple-500 flex items-center justify-center shadow-md"
+      >
+        <div class="flex flex-col">
+          <span class="text-4xl font-bold"> 143 </span>
+          <span> Students </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts"></script>
