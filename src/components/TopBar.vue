b7e92a9742316f07933796ac041dc006da154b15
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 14:22:50 +0100

    create topbar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex justify-between border-b border-gray-700 pl-14 pr-7 py-5 items-center"
+  >
+    <div class="flex space-x-5">
+      <i class="bi bi-search font-bold"></i
+      ><input
+        class="font-bold w-96 bg-transparent outline-none"
+        placeholder="Search"
+      />
+    </div>
+    <div class="flex space-x-3">
+      <div class="flex flex-col text-sm text-right justify-center">
+        <span class="font-bold">Abdulqudduus Babalola</span
+        ><span class="text-gray-500">Super Admin</span>
+      </div>
+      <div
+        style="background-image: url('../../public/placeholder.jpg')"
+        class="bg-cover h-12 w-12 rounded-lg shadow-md"
+      ></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
