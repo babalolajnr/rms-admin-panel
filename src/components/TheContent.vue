91c5b0be97e6b9116f63ee54b913d17103379546
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 13:55:46 +0100

    add top bar

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="grow border-x border-gray-700">
+    <div class="flex justify-between border-b border-gray-700 pl-14 pr-7 py-7">
+      <div class="flex space-x-5">
+        <i class="bi bi-search font-bold"></i
+        ><input
+          class="font-bold w-96 bg-transparent outline-none"
+          placeholder="Search"
+        />
+      </div>
+      <div>User</div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
