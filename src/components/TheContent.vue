f37df2ebf199a33c0049e91759cdf000d51b2814
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 14:23:56 +0100

    import topbar

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="grow border-x border-gray-700">
-    <div class="flex justify-between border-b border-gray-700 pl-14 pr-7 py-7">
-      <div class="flex space-x-5">
-        <i class="bi bi-search font-bold"></i
-        ><input
-          class="font-bold w-96 bg-transparent outline-none"
-          placeholder="Search"
-        />
-      </div>
-      <div>User</div>
-    </div>
+    <TopBar />
     <router-view></router-view>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TopBar from "./TopBar.vue";
+</script>
